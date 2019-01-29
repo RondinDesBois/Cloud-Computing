4a2a1df6a969e0b2736b6e23239bdc06778249f0
Sam Germain <sam.germain@metropolia.fi>
2019-01-29 11:28:28 +0000

    w3-1.2

--- w3 Vue/01 Introduction/logic.js
+++ w3 Vue/01 Introduction/logic.js
@@ -4,26 +4,43 @@
     return isNaN(value) || !isFinite(value);
   }
 
+	function addtext(){
+		if (document.createTextNode){
+			var textToAdd = localStorage.getItem('res')
+			var newNode = document.createTextNode(textToAdd);
+			document.getElementById("mydiv").appendChild(newNode);
+		} else {
+			alert("Looks like you have an old browser!");
+		}
+	}
 
 var calc = new Vue({
 	      el: '#app',
 	      data: { x: 0, y: 0, lastResult: 0 },
+
+	    //   watch: {
+			  //   results: {
+			  //     handler: function (results) {
+			  //       resultStorage.save(results)
+			  //     },
+			  //     deep: true
+			  //   }
+  			// },
 	      computed: {
 	        result: function() {
-	          let x = parseFloat(this.x);
-	          if(isNotNumericValue(x))
-	          return this.lastResult;
+	          // let x = parseFloat(this.x);
+	          // if(isNotNumericValue(x))
+	          // return this.lastResult;
 
-	          let y = parseFloat(this.y);
-	          if(isNotNumericValue(y))
-	          return this.lastResult;
-
-
+	          // let y = parseFloat(this.y);
+	          // if(isNotNumericValue(y))
+	          // return this.lastResult;
 	          //this.lastResult = x + y;
-
+						addtext()
 	          return this.lastResult;
 	        }
 	      },
+
 	      methods: {
 		      multiply: function () {
 
@@ -35,6 +52,14 @@ var calc = new Vue({
 	          if(isNotNumericValue(y))
 	          return this.lastResult;
 
+	          const aResult = {
+    					x: this.x,
+    					y: this.y,
+    					result: this.lastResult,
+    					opp: "multiply"
+						};
+						localStorage.setItem('res', JSON.stringify(aResult));
+
 						return this.lastResult = x*y;
 					},
 					divide: function () {
@@ -45,6 +70,15 @@ var calc = new Vue({
 	          let y = parseFloat(this.y);
 	          if(isNotNumericValue(y))
 	          return this.lastResult;
+
+	          const aResult = {
+    					x: this.x,
+    					y: this.y,
+    					result: this.lastResult,
+    					opp: "divide"
+						};
+						localStorage.setItem('res', JSON.stringify(aResult));
+
 
 						return this.lastResult = x/y;
 					}
